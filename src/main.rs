6f3672c2277fe28ddeea169bbf751bf8427abcce
Hungkhoaitay <e0550385@u.nus.edu>
2021-06-15 10:58:20 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod color;
+mod coordinate;
+mod points;
+mod ply_file;
+mod ply_dir;
+mod renderer;
+
 fn main() {
-    println!("Hello, world!");
+    // ply_dir::PlyDir::new("out").play();
+
+    let data = ply_file::PlyFile::new("longdress_vox10_1051.ply").read();
+
+    ply_file::PlyFile::create("write/1.ply").write_binary(data).unwrap();
 }
