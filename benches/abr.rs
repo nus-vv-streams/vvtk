44ac0f857e85f925f05cb44d5486b03b25f6de81
Bennett Clement <ben@benclmnt.com>
2023-03-24 16:30:06 +0800

    abr: bench quetra multiview

---
+++ benches/abr.rs
@@ -0,0 +1,34 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use vivotk::abr::quetra::QuetraMultiview;
+use vivotk::abr::RateAdapter;
+
+fn bench_quetra_multiview(c: &mut Criterion) {
+    c.bench_function("quetra-multiview", |b| {
+        let abr = QuetraMultiview::new(4, 30.0, 6, vec![1.72, 2.69, 3.61, 4.26, 4.47, 4.5]);
+        b.iter(|| {
+            abr.select_quality(
+                black_box(2),
+                black_box(1728450.56),
+                black_box(&[
+                    vec![827392, 1089536, 1490944, 2646016, 4972544, 8110080],
+                    vec![335872, 368640, 368640, 532480, 786432, 729088],
+                    vec![729088, 999424, 1466368, 2596864, 4767744, 7340032],
+                    vec![794624, 1048576, 1466368, 2547712, 4685824, 7872512],
+                    vec![270336, 303104, 319488, 442368, 704512, 679936],
+                    vec![802816, 1105920, 1572864, 2842624, 5349376, 7626752],
+                ]),
+                black_box(&[
+                    -0.07874092,
+                    -0.054728724,
+                    0.9953917,
+                    0.07874092,
+                    0.054728724,
+                    -0.9953917,
+                ]),
+            );
+        })
+    });
+}
+
+criterion_group!(benches, bench_quetra_multiview);
+criterion_main!(benches);
--- src/abr/mod.rs
+++ src/abr/mod.rs
@@ -51,16 +51,15 @@ impl MCKP {
         quality: f32,
         qualities_chosen: &mut Vec<usize>,
     ) -> (f32, Vec<usize>) {
-        if views_left == 0 {
-            dbg!(quality, &qualities_chosen);
-            return (quality, qualities_chosen.iter().rev().cloned().collect());
-        }
-
-        if network_throughput <= 0.0 {
+        if network_throughput < 1e-4 {
             return (f32::MIN, vec![]);
         }
 
-        let mut result = (0.0, vec![]);
+        if views_left == 0 {
+            return (quality, qualities_chosen.iter().rev().cloned().collect());
+        }
+
+        let mut result = (0.0, vec![0; available_bitrates.len()]);
         for (i, r) in available_bitrates[views_left - 1].iter().enumerate() {
             qualities_chosen.push(i);
             let (q, chosen) = self.select_quality_helper(
@@ -102,7 +101,6 @@ impl RateAdapter for MCKP {
             0.0,
             &mut v,
         );
-        dbg!(quality, &qualities_chosen);
         qualities_chosen
     }
 }
@@ -113,6 +111,13 @@ mod tests {
     #[test]
     fn test_mckp_select_quality() {
         let mckp = MCKP::new(6, vec![1.72, 2.69, 3.61, 4.26, 4.47, 4.5]);
+        let bitrate_sum = |qualities_chosen: &[usize], available_bitrates: &[Vec<u64>]| {
+            qualities_chosen
+                .iter()
+                .zip(available_bitrates)
+                .map(|(i, v)| v[*i])
+                .sum::<u64>() as f64
+        };
         let available_bitrates = vec![
             vec![133, 182, 323, 607, 990],
             vec![45, 45, 65, 96, 89],
@@ -121,50 +126,50 @@ mod tests {
             vec![37, 39, 54, 86, 83],
             vec![125, 192, 347, 653, 931],
         ];
+        let qualities = mckp.select_quality(
+            0,
+            750.0,
+            &available_bitrates,
+            &[0.88, 0.17, 0.44, -0.94, 0.25, -0.17],
+        );
+        assert_eq!(qualities, vec![0, 2, 0, 1, 2, 1]);
+        assert!(bitrate_sum(&qualities, &available_bitrates) <= 750.0);
+
+        let qualities = mckp.select_quality(
+            0,
+            1000.0,
+            &available_bitrates,
+            &[0.88, 0.17, 0.44, -0.94, 0.25, -0.17],
+        );
+        assert_eq!(qualities, vec![0, 1, 0, 2, 1, 2]);
+        assert!(bitrate_sum(&qualities, &available_bitrates) <= 1000.0);
+
+        let qualities = mckp.select_quality(
+            0,
+            1500.0,
+            &available_bitrates,
+            &[0.88, 0.17, 0.44, -0.94, 0.25, -0.17],
+        );
+        assert_eq!(qualities, vec![0, 4, 0, 3, 4, 2]);
+        assert!(bitrate_sum(&qualities, &available_bitrates) <= 1500.0);
+
         assert_eq!(
             mckp.select_quality(
                 0,
-                750.0,
+                500.0,
                 &available_bitrates,
-                &[0.88, 0.17, 0.44, -0.94, 0.25, -0.17]
+                &[-0.18, 0.82, 0.53, 0.96, -0.20, 0.14]
             ),
-            vec![0, 2, 0, 2, 2, 1]
+            vec![0, 0, 0, 0, 0, 0]
         );
         assert_eq!(
             mckp.select_quality(
                 0,
                 1000.0,
                 &available_bitrates,
-                &[0.88, 0.17, 0.44, -0.94, 0.25, -0.17]
-            ),
-            vec![0, 2, 0, 3, 1, 1]
-        );
-        assert_eq!(
-            mckp.select_quality(
-                0,
-                1500.0,
-                &available_bitrates,
-                &[0.88, 0.17, 0.44, -0.94, 0.25, -0.17]
-            ),
-            vec![0, 4, 0, 3, 2, 3]
-        );
-        assert_eq!(
-            mckp.select_quality(
-                0,
-                600.0,
-                &available_bitrates,
                 &[-0.18, 0.82, 0.53, 0.96, -0.20, 0.14]
             ),
-            vec![4, 0, 0, 0, 4, 1]
-        );
-        assert_eq!(
-            mckp.select_quality(
-                0,
-                1000.0,
-                &available_bitrates,
-                &[-0.18, 0.82, 0.53, 0.96, -0.20, 0.14]
-            ),
-            vec![4, 0, 0, 0, 4, 2]
+            vec![2, 0, 0, 0, 4, 1]
         );
         assert_eq!(
             mckp.select_quality(
@@ -173,7 +178,7 @@ mod tests {
                 &available_bitrates,
                 &[-0.18, 0.82, 0.53, 0.96, -0.20, 0.14]
             ),
-            vec![4, 0, 0, 0, 4, 4]
+            vec![3, 0, 0, 0, 4, 2]
         );
     }
 }
