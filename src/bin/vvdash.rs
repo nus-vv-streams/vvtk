808009283a4ff336a8293647a51bd19cb41bb9e2
WangGLJoseph <joseph.cnsg@gmail.com>
2023-03-30 15:00:13 +0800

    work with 5 qualities R01-R05

--- src/bin/vvdash.rs
+++ src/bin/vvdash.rs
@@ -1,79 +1,161 @@
 use clap::Parser;
+use regex::Regex;
+use std::fs;
 use std::fs::copy;
-use std::fs::read_dir;
 use std::fs::File;
-use std::fs::ReadDir;
+use std::io;
+use std::path::Path;
 use std::path::PathBuf;
 
-// take files from input folder and a simulated network condition,
-// then output files of varying qualities into output folder
+// take binary files from input folder and a simulated network condition,
+// then output binary files of varying qualities into output folder (should decoding be done here?)
 #[derive(Parser)]
 struct Args {
     #[clap(parse(from_os_str))]
     input_path: PathBuf,
     output_path: PathBuf,
     network_path: PathBuf,
-    frames: usize,
+    algorithm: String,
+    throughput_estimation: String,
+}
+
+fn get_filename(filepath: &Path) -> io::Result<()> {
+    // Get all files in target directory.
+    // Replace "." with a more useful directory if needed.
+    for entry in fs::read_dir(filepath)? {
+        let path = entry?.path();
+        // Get path string.
+        let path_str = path.to_str().unwrap();
+        println!("PATH: {}", path_str);
+    }
+    Ok(())
+}
+
+fn get_entries(filepath: &Path) -> io::Result<Vec<PathBuf>> {
+    let mut entries = fs::read_dir(filepath)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    // The order in which `read_dir` returns entries is not guaranteed. If reproducible
+    // ordering is required the entries should be explicitly sorted.
+
+    entries.sort();
+
+    // The entries have now been sorted by their path.
+
+    Ok(entries)
 }
 
 fn main() {
-    // e.g. vvdash ./input ./output ./simulated_network.txt 300
-    // vvdash [input_path] [output_path] [network_conditions] [number_of_frames]
     let args: Args = Args::parse();
     let input_path = args.input_path;
     let output_path = args.output_path;
     let network_path = args.network_path;
-    let total_frames = args.frames;
+    let algorithm = args.algorithm;
+    let throughput_estimation = args.throughput_estimation;
+    let start_no: usize;
 
     // reading network conditions
     let network_content =
         std::fs::read_to_string(network_path).expect("could not read network file");
-    let mut bandwidth: Vec<i32> = Vec::new();
+    // using f32 for bandwidth in KB/s
+    let mut bandwidth: Vec<f32> = Vec::new();
     for line in network_content.lines() {
         bandwidth.push(line.parse().unwrap());
     }
 
+    let mut starting_frame_int: usize = 0;
+    let mut frame_increment_int: usize = 0;
     let mut count: usize = 0;
+    let mut total_frames: usize = 0;
+    let extension = "pcd";
 
-    // assume default input folder is always the "hi" folder
-    let mut input_folder_hi = input_path.clone();
-    input_folder_hi.push("hi");
-    let mut input_folder_lo = input_path;
-    input_folder_lo.push("lo");
-    let mut input_folder: ReadDir;
+    let mut input_folder_R01 = input_path.clone();
+    input_folder_R01.push(format!("{}", "R01"));
+    let mut input_folder_R02 = input_path.clone();
+    input_folder_R02.push(format!("{}", "R02"));
+    let mut input_folder_R03 = input_path.clone();
+    input_folder_R03.push(format!("{}", "R03"));
+    let mut input_folder_R04 = input_path.clone();
+    input_folder_R04.push(format!("{}", "R04"));
+    let mut input_folder_R05 = input_path.clone();
+    input_folder_R05.push(format!("{}", "R05"));
+    // let mut input_folder: ReadDir;
     let mut input_folder_pathbuf: &PathBuf;
 
-    while count < total_frames {
-        // arbitrary separation between lo and hi quality: 150KB/s
-        if bandwidth[count] < 150 {
-            input_folder = read_dir(&input_folder_lo).unwrap();
-            input_folder_pathbuf = &input_folder_lo;
-        } else {
-            input_folder = read_dir(&input_folder_hi).unwrap();
-            input_folder_pathbuf = &input_folder_hi;
-        }
+    // getting entries from default input folder ("R05" folder) to retrieve infomation (any alternatives?)
+    let entries = get_entries(input_folder_R05.as_path()).expect("failed to get entries");
 
-        // get the correct frame from the input folder
-        let input_frame_name: PathBuf = input_folder.nth(count).unwrap().ok().unwrap().path();
+    let re = Regex::new(r"(.{7})(.{3})_(.{3})_(.{3})_(\d{4}).pcd").unwrap();
 
-        let mut input_frame = input_folder_pathbuf.clone();
-        input_frame.push(input_frame_name.file_name().unwrap());
-        let mut output_frame = output_path.clone();
-        output_frame.push(input_frame_name.file_name().unwrap());
-        let _o = File::create(&output_frame);
-        copy(&input_frame, &output_frame).unwrap_or_else(|_| {
-            panic!(
-                "failed to copy from {} to {}",
-                &input_frame.display(),
-                &output_frame.display()
-            )
-        });
+    let first_entry_filename = entries[0].as_path().to_str().unwrap();
+    let first_entry_filename_short =
+        &first_entry_filename[(input_folder_R05.as_path().to_str().unwrap().chars().count() + 1)..]; // + 1 for the slash /
+    println!("First entry filename: {}", first_entry_filename_short);
+    assert!(re.is_match(first_entry_filename_short)); // panics if file name not a match, able to input regex as CLI params?
 
-        count += 1;
+    // S25C2AIR05_F30_rec_0536.pcd -> [R05] [F30] [0536] information needed for decoding are retrieved from file name
+    for cap in re.captures_iter(first_entry_filename_short) {
+        let prefix = &cap[1].to_owned();
+        let rate = &cap[2].to_owned();
+        let frame_count = &cap[3].to_owned();
+        let starting_frame = &cap[5].to_owned();
+
+        // frame_count is 'F30', substring
+        frame_increment_int = 1;
+        starting_frame_int = starting_frame.parse().unwrap();
+        total_frames = entries.len() * frame_increment_int;
     }
 
-    // major issue with implementation: readdir() does not guarentee the order (alphabetical),
-    // may need to experiment with reading into a Vec and then sorting the files
-    // order of arrival of frames matter when taken into a buffer during streaming
-    // https://stackoverflow.com/questions/40021882/how-to-sort-readdir-iterator
+    start_no = starting_frame_int;
+
+    while count < total_frames {
+        let quality: &str;
+        // buffer-based approach used for rate adaptation, appropriate lower and higher reservoir
+        // needed in order to avoid overflow and underflow
+        let mut bandwidth_buf: f32 = 0.0;
+        for i in count..count + frame_increment_int {
+            bandwidth_buf += bandwidth[i];
+        }
+
+        // a simplistic average value is chosen (4411.4, size in KB of 1s of R05 binaries),
+        // additional research needed to determine appropriate lower and higher reservoir values
+        // for simulation purposes, use the .bin file sizes as benchmark for values (naive algo)
+        if bandwidth_buf < 120.0 {
+            // input_folder = read_dir(&input_folder_lo).unwrap();
+            input_folder_pathbuf = &input_folder_R01;
+            quality = "R01";
+        } else {
+            // input_folder = read_dir(&input_folder_hi).unwrap();
+            input_folder_pathbuf = &input_folder_R05;
+            quality = "R05";
+        }
+
+        // take and use the regex input from CLI, format of frame name to be considered:
+        // can extract out regex of format 'S25C2AI' to be an input param in the future (-f flag?)
+        let in_frame_name = format!(
+            "S25C2AI{}_F30_rec_{}.{}",
+            quality,
+            format!("{:0>4}", count + start_no),
+            extension
+        );
+
+        let out_frame_name = format!("out_{}_{}.{}", count, quality, extension);
+
+        let mut input_frame = input_folder_pathbuf.clone();
+        input_frame.push(&in_frame_name);
+        let mut output_frame = output_path.clone();
+        output_frame.push(&out_frame_name);
+        let _o = File::create(&output_frame);
+        copy(&input_frame, &output_frame).expect(&format!(
+            "failed to copy from {} to {}",
+            &input_frame.display(),
+            &output_frame.display()
+        ));
+
+        count += frame_increment_int;
+    }
+
+    // decoding after binaries are in out folder (should be separate function?)
+    //
 }
